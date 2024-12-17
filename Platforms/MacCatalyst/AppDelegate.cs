44cb54c0e7881da83de18da189d0e71d44de701f
prayashjungkhadka2005 <np05cp4a220088@iic.edu.np>
2024-12-17 09:38:59 +0545

    first commit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BudgetMate
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
