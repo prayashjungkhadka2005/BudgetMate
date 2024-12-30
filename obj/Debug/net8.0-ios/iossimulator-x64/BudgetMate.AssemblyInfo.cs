bc4e80d1e818e9e8d28087501c78d5cf5d6855f5
prayashjungkhadka2005 <np05cp4a220088@iic.edu.np>
2024-12-30 09:34:56 +0545

    Fixed Error

--- obj/Debug/net8.0-ios/iossimulator-x64/BudgetMate.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/BudgetMate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+08cb46d846eae701f674e803b6a4185d59cc0375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2bb0decff37e18807fcfc30a06aa44128ec15ef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
