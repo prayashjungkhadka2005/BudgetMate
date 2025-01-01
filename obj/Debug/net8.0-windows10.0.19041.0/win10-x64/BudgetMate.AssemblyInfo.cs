a5e56f57239f4dcc807ff705c502736d5ae504e3
prayashjungkhadka2005 <np05cp4a220088@iic.edu.np>
2025-01-01 18:54:14 +0545

    Update UI

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BudgetMate.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BudgetMate.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2bb0decff37e18807fcfc30a06aa44128ec15ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bc4e80d1e818e9e8d28087501c78d5cf5d6855f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
