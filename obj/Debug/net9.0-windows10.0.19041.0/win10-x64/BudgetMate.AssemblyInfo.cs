07082e53b30d9799438de3186bac731735261e5b
prayashjungkhadka2005 <np05cp4a220088@iic.edu.np>
2025-01-13 15:59:39 +0545

    Updated css

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/BudgetMate.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/BudgetMate.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b93e27332645ac6ec4a215320c702ce32e183e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d7fbff5a609bb180cc4309c48b672201b0f4cdb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
